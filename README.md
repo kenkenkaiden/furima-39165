a07dbafa61563f32523403ebe05b06dc928c9bf7
kenkenkaiden <allexhard2000@gmail.com>
2023-07-22 21:22:50 +0900

    ユーザーの新規登録機能の実装

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 | Column             | Type   | Options                       |
 | ------------------ | ------ | ----------------------------- |
 | nickname           | string | null: false                   |
-| email              | string | null: false, unique: true |
+| email              | string | null: false, unique: true     |
 | encrypted_password | string | null: false                   |
 | family_name        | string | null: false                   |
 | last_name          | string | null: false                   |
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, null: false
+  validates :email, null: false, uniqueness: true
+  validates :encrypted_password, null: false
+  validates :family_name, null: false
+  validates :last_name, null: false
+  validates :family_name_kana, null: false
+  validates :first_name_kana, null: false
+  validates :birthday, null: false
 end
