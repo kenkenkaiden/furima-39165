5843d844d0458a9be672f30cdeee7d405866cd3b
kenkenkaiden <allexhard2000@gmail.com>
2023-07-24 17:56:11 +0900

    新規登録/ユーザー情報及び本人情報確認のバリデーションの付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validate :password_format
+
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角（カタカナ）で入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角（カタカナ）で入力してください" }
   validates :birthday, presence: true
+
+  def password_format
+    return if password.blank?
+
+    unless password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/)
+      errors.add(:password, 'は半角英数字混合で入力してください')
+    end
+  end
 end
